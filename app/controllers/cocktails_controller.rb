dae1898fda1a8a653e20f5848523a62ab7f1f921
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-15 18:19:43 +0800

    Commit css updates on cards, rows, and ingredient cards

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,6 +7,13 @@ class CocktailsController < ApplicationController
 
   def edit
     @cocktail = Cocktail.find(params[:id])
+
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-resources :cocktails, only: [:index, :show, :new, :create, :edit] do
+resources :cocktails, only: [:index, :show, :new, :create, :edit, :update] do
   resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
